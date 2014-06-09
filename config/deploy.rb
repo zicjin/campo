21edc378a7eafe89cfff4e0468d91acdeb9eb209
cheney <zicjin@gmail.com>
2014-06-09 18:07:34 +0800

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'baozou-dis'
+set :application, 'campo'
 set :repo_url, 'git@github.com:zicjin/campo.git'
 set :deploy_to, -> { "/var/www/#{fetch(:application)}" }
 set :rails_env, 'production'
--- script/setup.sh
+++ script/setup.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-USER=`zcj`
-APP_NAME=`baozou-dis`
-APP_ROOT=/var/www/$APP_NAME
+USER=`whoami`
+APP_ROOT=/var/www/campo
 
 sudo apt-get update
 
 # Install system packages
-# DEBIAN_FRONTEND=noninteractive 非交互式
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y redis-server memcached git-core nodejs imagemagick postfix
 
 # Install Elasticsearch
@@ -22,9 +20,9 @@ sudo service elasticsearch start
 sudo apt-get install -y postgresql libpq-dev
 sudo su postgres -c "createuser -d -R -S $USER"
 
-# Install Passenger. 支持ruby的web服务器。
+# Install Passenger
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates #-y, --yes Assume yes to all queries
+sudo apt-get install -y apt-transport-https ca-certificates
 sudo bash -c "echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger precise main' > /etc/apt/sources.list.d/passenger.list"
 sudo apt-get update
 sudo apt-get install -y nginx-extras passenger
@@ -50,20 +48,19 @@ mkdir -p $APP_ROOT/shared/config
 cp config/database.example.yml $APP_ROOT/shared/config/database.yml
 cp config/secrets.example.yml $APP_ROOT/shared/config/secrets.yml
 cp config/config.example.yml $APP_ROOT/shared/config/config.yml
-# sed是非交互式的编辑器，逐行处理文件，并将结果发送到屏幕。默认不修改原始文件，-i表示直接对目标文件操作，-n仅显示处理后的结果
-sed -i "s/secret_key_base: \w\+/secret_key_base: `bundle exec rake secret`/g" $APP_ROOT/shared/config/secrets.yml #s/x/y/ 表示x替换为y字符串。g表示当前行全局匹配
+sed -i "s/secret_key_base: \w\+/secret_key_base: `bundle exec rake secret`/g" $APP_ROOT/shared/config/secrets.yml
 
 # Resque init script
 sudo cp config/resque.example.sh /etc/init.d/resque
 sudo chmod +x /etc/init.d/resque
-sudo sed -i "s|APP_ROOT=.\+|APP_ROOT=$APP_ROOT/current|" /etc/init.d/resque #使用 |……|……| 不需要转义'/'符号
+sudo sed -i "s|APP_ROOT=.\+|APP_ROOT=$APP_ROOT/current|" /etc/init.d/resque
 sudo sed -i "s/USER=\w\+/USER=$USER/" /etc/init.d/resque
 sudo update-rc.d resque defaults
 
 # Nginx config
-sudo cp config/nginx.example.conf /etc/nginx/sites-available/$APP_NAME
-sudo sed -i "s|root .\+;|root $APP_ROOT/current/public;|" /etc/nginx/sites-available/$APP_NAME
-sudo ln -s /etc/nginx/sites-available/$APP_NAME /etc/nginx/sites-enabled
+sudo cp config/nginx.example.conf /etc/nginx/sites-available/campo
+sudo sed -i "s|root .\+;|root $APP_ROOT/current/public;|" /etc/nginx/sites-available/campo
+sudo ln -s /etc/nginx/sites-available/campo /etc/nginx/sites-enabled
 sudo rm /etc/nginx/sites-enabled/default
 sudo sed -i 's/# passenger_root/passenger_root/' /etc/nginx/nginx.conf
 sudo sed -i "s|# passenger_ruby .\+;|passenger_ruby /home/$USER/.rvm/wrappers/default/ruby;|" /etc/nginx/nginx.conf
