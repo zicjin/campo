839055722b4641aa8f4880c95a9688e5f18b0b68
Cheney Jin <zicjin@gmail.com>
2014-06-28 07:12:09 +0000

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
   config.action_controller.asset_host = Proc.new { |source, request|
-    if source =~ /respond\.proxy-.+(js|gif)$/
+    if source =~ /respond\.proxy-.+html$/
       "//baozoubisai.com"
     end
   }
