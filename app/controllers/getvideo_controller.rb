8f4a174ee48548389b856d5c6fd38f49e067bc35
Cheney Jin <zicjin@gmail.com>
2014-07-01 09:19:22 +0000

    .

--- app/controllers/getvideo_controller.rb
+++ app/controllers/getvideo_controller.rb
@@ -2,8 +2,7 @@ class GetvideoController < ApplicationController
   before_action :login_required
 
   def index
-    _video = Getvideo.parse params['url']
-
-    render json: { flash: @video.flash }
+    video = Getvideo.parse params['url']
+    render json: { flash: video.flash }
   end
 end
